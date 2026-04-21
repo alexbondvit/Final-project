412beb530f53aacb604cc2969b431d3c971352a8
arina-cmdv <kekshukarina@gmail.com>
2026-04-21 23:23:36 +0200

    added checkout page & finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,33 @@
-import logo from './assets/images/Group 13 (2).png';
-import samokat from './assets/images/Байк.png';
-import foto from './assets/images/Rectangle 23.png';
-import foto1 from './assets/images/Rectangle 24.png';
-import art from './assets/images/Path.png'; 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/about";
+//import Xiaomi from "./pages/settings";
+import Checkout from "./pages/checkout";
+
+
+function Reviews() {
+  return (
+    <div>
+      <h1>Отзывы и FAQ</h1>
+    </div>
+  );
+}
 
 function App() {
 
   return (
     <>
-      <div className="ellipse"></div>
-      <img src={logo} className='Logo'/>
-      <button className='buy'>Купить</button>
-      <nav className='nav'>
-        {/* <Link to="/" className="">О продукте</Link>
-        <Link to="/">2</Link>
-        <Link to="/">3</Link> */}
-        <h2 className='title-about'>О продукте</h2>
-        <h2 className='title-settings'>Вид и безопасность</h2>
-        <h2 className='title-reviews'>Отзывы и FAQ</h2>
-      </nav>
-      <h1 className='title'>Стильный электросамокат <br/>с мощным аккумулятором</h1>
-      <h3 className='text1'>Улучшенная производительность <br/>для дальних путешествий. <br/>Работает так же просто, <br/>как и выглядит.</h3>
-      <img src={samokat} className='Samokat'/>
-      <h2 className='text2'>Мощная батарея и экономичный расход <br/>заряда позволяют преодолевать <br/>расстояния до 45 км</h2>
-      <div className='block1'>
-        <h3>Три режима скорости</h3>
-        <hr className='line'/>
-        <p>Три режима езды позволяют почуствовать себя комфортно в любой ситуации. Режим «S» увеличитвает 
-          скорость, если опаздываете на работу. <br/>Во время прогулки по парку активируйте режим «D». <br/>Включите режим «Пешеход», когда двигаетесь <br/>по оживлённой улице.</p>
-      </div>
-      <div className='block2'>
-        <h3>Система восстановления энергии</h3>
-        <hr/>
-        <p>Во время торможения и движения по инерции система преобразовывает 
-          кинетическую энергию <br/>в электрическую, позволяя увеличить общий <br/>запас хода самоката.</p>
-      </div>
-      <div className='block3'>
-        <h3>Мощная батарея</h3>
-        <hr/>
-        <p>Самокат оснащен аккумулятором емкостью <br/>12 800 мАч. Это позволяет преодолевать расстояния <br/>до 45 км без подзарядки.</p>
-      </div>
-      <img src={foto} className='foto'/>
-      <img src={foto1} className='foto1'/>
-      <img src={art} className='art'/>
+    <Router>
+      <Routes>
+        {/* Головна сторінка (твій код) за адресою "/" */}
+        <Route path="/" element={<Home />} />
+
+        <Route path="/reviews" element={<Reviews />} />
+
+        {/* <Route path="/safety" element={<Xiaomi />} /> */}
+
+        <Route path="/checkout" element={<Checkout/>}/>
+      </Routes>
+      </Router>
     </>
   )
 }
